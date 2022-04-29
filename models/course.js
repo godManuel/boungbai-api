7663fcf218d9293f3449c6ed770de58596a5391e
BoungbaiComputers <justlyjohn198@gmail.com>
2022-04-29 13:12:59 +0100

    Refacted course.js

--- models/course.js
+++ models/course.js
@@ -1,62 +1,70 @@
-const slugify = require('slugify')
-const Joi = require('joi');
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+const slugify = require("slugify");
+const Joi = require("joi");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const courseSchema = new Schema({
+const courseSchema = new Schema(
+  {
     name: {
-        type: String, 
-        enum: ['HTML', 'CSS', 'Bootstrap', 'Javascript', 'React', 'Node']
+      type: String,
+      enum: ["HTML", "CSS", "Bootstrap", "Javascript", "React", "Node"],
     },
     slug: {
-        type: String, 
-        required: true, 
-        unique: true
-      },
+      type: String,
+      required: true,
+      unique: true,
+    },
     author: {
-        type: String, 
-        required: true,
-        minlength: 3, 
-        maxlength: 255
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 255,
     },
     price: {
-        type: Number,
-        min: 500,
-        max: 1000,
-        required: true
-    }, 
+      type: Number,
+      min: 500,
+      max: 1000,
+      required: true,
+    },
     image: {
-        type: String
+      type: String,
     },
     isPublished: {
-        type: Boolean, 
-        default: false
+      type: Boolean,
+      default: false,
     },
     category: {
-        type: String, 
-        enum: ['Microsoft Office Suite', 'Graphic Design', 'Web Development & Hosting', 'Penetration Testing']
-    }
-}, { timestamps: true })
+      type: String,
+      enum: [
+        "Microsoft Office Suite",
+        "Graphic Design",
+        "Web Development & Hosting",
+        "Penetration Testing",
+      ],
+    },
+  },
+  { timestamps: true }
+);
 
-const Course = mongoose.model('Course', courseSchema);
+const Course = mongoose.model("Course", courseSchema);
 
-// Slugify our category-name to get a slug value 
-courseSchema.pre('validate', function(){
-    if(this.name) {
-      this.slug = slugify(this.name, { lower: true, strict: true })
-    }
-  })
+// Slugify our category-name to get a slug value
+courseSchema.pre("validate", function () {
+  if (this.name) {
+    this.slug = slugify(this.name, { lower: true, strict: true });
+  }
+});
 
 const validateCourse = (course) => {
-    const schema = Joi.object({
-        name: Joi.string().min(5).max(50).required(),
-        author: Joi.string().min(5).max(50).required(),
-        price: Joi.number().min(500).max(1000).required(),
-        category: Joi.string().required()
-    })
+  const schema = Joi.object({
+    name: Joi.string().min(5).max(50).required(),
+    author: Joi.string().min(5).max(50).required(),
+    price: Joi.number().min(500).max(1000),
+    category: Joi.string().required(),
+  });
 
-    return schema.validate(course);
-}
+  return schema.validate(course);
+};
 
 exports.validate = validateCourse;
 exports.Course = Course;
